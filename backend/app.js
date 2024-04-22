64ca9d9ebdb7b7323b58dc656122b9060d21a8f5
Benjamin Mazars <mazars.benjamin@hotmail.fr>
2024-04-22 16:27:01 +0200

    fin cour node partie2

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,16 @@
 const express = require("express");
-const bodyParser = require()
+const bodyParser = require("body-parser")
 const mongoose = require('mongoose');
 
 
-
+const Thing = require("./models/Thing");
 
 mongoose.connect("mongodb+srv://benjaminmazars:sEtPjCJwxvSK6Gj2@cluster0.dvaku3x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 const app = express();
 
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -18,35 +19,43 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use(express.json());
+app.use(bodyParser.json());
+
+
 
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message:"Objet cree"
-    })
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
 });
 
+app.put("/api/stuff/:id", (req, res, next) => {
+  Thing.updateOne({_id: req.params.id}, {...req.body, _id:req.params.id})
+    .then (() => res.status(200).json({message: "objet modifie"}))
+    .catch((error => res.status(400).json({ error })));
+})
+
+app.delete ("/api/stuff/:id", (req, res, next) => {
+    Thing.deleteOne({_id: req.params.id})
+      .then(() => res.status(200).json({message: "objet supprime"}))
+      .catch((error => res.status(400).json({ error })));
+})
+
+app.get("/api/stuff/:id", (req, res, next) => {
+    Thing.findOne({_id: req.params.id})
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+}); 
+
 app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
   });
 
 
